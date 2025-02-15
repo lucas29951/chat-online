ff7cca1e815f9e555bd1699a038be1ebcf220e70
Lucas Rodriguez <lucasg29r95@gmail.com>
2025-02-15 17:26:28 -0300

    Correccion de funciones de emision de mensajes usando el nuevo script de formato

--- server.js
+++ server.js
@@ -2,6 +2,10 @@ const express = require('express');
 const path = require('path');
 const http = require('http');
 const socketio = require('socket.io');
+const formatMessage = require('./utils/messages');
+
+const PORT = 3000 || process.env.PORT;
+const botName = 'Admin Bot';
 
 const app = express();
 const server = http.createServer(app);
@@ -10,22 +14,20 @@ const io = socketio(server);
 app.use(express.static(path.join(__dirname, 'public')));
 
 io.on('connection', socket => {
-    console.log('Nueva conexion...');
+    // console.log('Nueva conexion...');
 
-    socket.emit('message', 'Bienvenido al Chat Online!');
+    socket.emit('message', formatMessage(botName, 'Bienvenido al Chat Online!'));
 
-    socket.broadcast.emit('message', 'Un usuario se unio al chat');
+    socket.broadcast.emit('message', formatMessage(botName, 'Un usuario se unio al chat'));
 
     socket.on('disconnect', () => {
-        io.emit('message', 'Un usuario salio del chat');
+        io.emit('message', formatMessage(botName, 'Un usuario salio del chat'));
     });
 
     socket.on('chatMessage', (msg) => {
         // console.log(msg);
-        io.emit('message', msg);
+        io.emit('message', formatMessage('USER', msg));
     });
 });
-
-const PORT = 3000 || process.env.PORT;
 
 server.listen(PORT, () => console.log(`Server ejecutandose en el puerto ${PORT}`));
