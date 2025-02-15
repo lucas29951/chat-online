c40c216e9cce55d597c1003df53798140834483c
Lucas Rodriguez <lucasg29r95@gmail.com>
2025-02-15 18:22:09 -0300

    Funciones agregadas para la carga del DOM con informacion de las salas

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,8 @@
 
 const chatForm = document.getElementById('chat-form');
 const chatMessages = document.querySelector('.chat-messages');
+const roomName = document.getElementById('room-name');
+const usersList = document.getElementById('users');
 
 const { username, room } = Qs.parse(location.search, {
     ignoreQueryPrefix: true
@@ -10,6 +12,11 @@ const { username, room } = Qs.parse(location.search, {
 const socket = io();
 
 socket.emit('joinRoom', { username, room });
+
+socket.on('roomUsers', ({ room, users }) => {
+    outputRoomName(room);
+    outputUsers(users);
+});
 
 socket.on('message', message => {
     outputMessage(message);
@@ -34,4 +41,14 @@ function outputMessage(message) {
     div.innerHTML = `<p class="meta">${message.username} <span>${message.time}</span></p>
 					 <p class="text">${message.text}</p>`;
     document.querySelector('.chat-messages').appendChild(div);
+}
+
+function outputRoomName(room) {
+    roomName.innerText = room;
+}
+
+function outputUsers(users) {
+    usersList.innerHTML = `
+    ${users.map(user => `<li>${user.username}</li>`).join('')}
+    `;
 }
